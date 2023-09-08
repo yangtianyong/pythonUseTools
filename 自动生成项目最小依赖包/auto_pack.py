64d2417e22930bacf8cb2ad9e4c20a9e76843e6e
tianyong.yang <tianyong.yang@guigu.com>
2023-09-08 14:48:04 +0800

    自动生成项目最小化依赖包

---
+++ 自动生成项目最小依赖包/auto_pack.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    Author:1112
+    Date:2023-09-08
+    Update: ---
+    Achieve:自动生成项目所需要的最小化的依赖包
+"""
+
+'''
+    方法一：使用pipreqs库
+         下载：pip install pipreqs  -- 进入该项目的根目录  -- pipreqs ./ --encoding=utf8
+         
+     方法二： 使用项目管理工具  Poetry -- 更多信息可以自行百度
+         
+'''
