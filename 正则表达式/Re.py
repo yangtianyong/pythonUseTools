07cc9c53e5ac7886a3bbedfcb1f695d5d6ab6ea7
tianyong.yang <tianyong.yang@guigu.com>
2023-09-11 10:33:28 +0800

    正则表达式知识记录

--- 正则表达式/Re.py
+++ 正则表达式/Re.py
@@ -6,6 +6,7 @@
     Achieve:正则表达式相关的内容
 """
 
+import re
 '''
     链接：
         https://docs.python.org/2/howto/regex.html
@@ -36,13 +37,44 @@
 '''
 
 '''
-    贪婪模式：匹配字符串时尝试匹配尽可能多的字符
-    非贪婪模式：匹配字符串时尝试匹配尽可能少的字符
-    一般在 * + {m,n} 等后面加上?
+    分组：
+    链接：https://zhuanlan.zhihu.com/p/375659011
+    用圆括号()括起来的正则表达式，匹配出来的内容表示一个分组
+    (exp):把括号内的正则作为一个分组，系统自动分配组号，可以通过分组号引用该分组
+    (?P<name>exp):定义一个命名分组，分组的正则是exp,通过分组名和分组号引用该分组
+    正则表达式引擎会默认分配一个编号，从1开始
+    可以通过引用分组的方式对重复出现的文本进行匹配
+'''
+
+string  = "现在是北京时间9点40分"
+pattern = re.compile(r'\D*(\d{1,2})\D*(\d{1,2})\D*')
+# pattern = re.compile(r'\D*(?P<hour>\d{1,2})\D*(?P<minute>\d{1,2})\D*')
+result = pattern.match(string)
+print(result.groups()[0])
+# print(result.groupdict())
+
+'''
+    修饰符：
+        i      不区分大小写
+        g      全局匹配
+        m      多行匹配
 '''
 
 '''
-    分组：
-    链接：https://zhuanlan.zhihu.com/p/375659011
-    用圆括号()括起来的正则表达式，匹配出来的内容表示一个分组。
+    re模块：
+    链接：https://blog.csdn.net/guo_qingxia/article/details/113979135
+        1>re.match(pattern,string,flags=0)
+            flags: re.I 忽略大小写  re.M 多行模式
+            
+        2>re.complie(pattern)
+            用于编译正则表达式，生成对象，供match()和search()使用
+            
+        3>re.search()
+            扫描整个字符串并返回第一个成功的匹配，如果没有匹配，就返回一个None
+            re.match()只匹配字符串的开始
+            
+        4>re.sub(pattern,repl,string,count=0,flags=0)
+        
+        5>re.split()
+            
 '''
